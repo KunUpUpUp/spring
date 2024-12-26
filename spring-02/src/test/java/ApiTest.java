51075f10bdb9176ee90dfb1bb62c34d3f4bd3549
KunUpUpUp <826580243@qq.com>
2024-12-26 14:12:38 +0800

    2

--- spring-02/src/test/java/ApiTest.java
+++ spring-02/src/test/java/ApiTest.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ApiTest {
+import bean.UserSerivce;
+import com.seasugar.factory.config.BeanDefinition;
+import com.seasugar.factory.support.DefaultListableBeanFactory;
+import org.junit.Test;
+
+public class ApiTest {
+    @Test
+    public void test_BeanFactory(){
+        // 1.初始化 BeanFactory
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        // 2.注册 bean
+        BeanDefinition beanDefinition = new BeanDefinition(UserSerivce.class);
+        beanFactory.registerBeanDefinition("userService", beanDefinition);
+
+        // 3.第一次获取 bean
+        UserSerivce userService = (UserSerivce) beanFactory.getBean("userService");
+        userService.queryUserInfo();
+
+        // 4.第二次获取 bean from Singleton
+        UserSerivce userService_singleton = (UserSerivce) beanFactory.getSingleton("userService");
+        userService_singleton.queryUserInfo();
+    }
 }
